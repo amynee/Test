3e944e24514c60a66e0b183049e90a709c19907b
Amyyne <tlijaniamine9@gmail.com>
2021-01-08 22:42:41 +0100

    Add models

--- backend/product/models.py
+++ backend/product/models.py
@@ -3,8 +3,36 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    PRDName = models.CharField(max_length=100)
-    PRDDesc = models.TextField()
-    PRDPrice = models.DecimalField(max_digits=5, decimal_places=2)
-    PRDCost = models.DecimalField(max_digits=5, decimal_places=2)
-    PRDCreated = models.DateTimeField()
+    PRDName = models.CharField(max_length=100, verbose_name="Product Name")
+    PRDDesc = models.TextField(verbose_name="Product Description")
+    PRDPrice = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Price")
+    PRDCost = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Cost")
+    PRDCreated = models.DateTimeField(verbose_name="Created At")
+
+    class Meta:
+        verbose_name="Product"
+        verbose_name_plural="Products"
+
+    def __str__(self):
+        return self.PRDName
+
+class ProductImage(models.Model):
+    PRDIproduct = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Product")
+    PRDIImage = models.ImageField(upload_to="product/", verbose_name="Image")
+
+    def __str__(self):
+        return str(self.PRDIproduct)
+
+class Category(models.Model):
+    CATName = models.CharField(max_length=100, verbose_name="Category Name")
+    CATParent = models.ForeignKey('self',limit_choices_to={'CATParent__isnull' : True}, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Category Parent")
+    CATDesc = models.TextField(verbose_name="Category Description")
+    CATImg = models.ImageField(upload_to='categry/', verbose_name="Category Image")
+
+    class Meta:
+        verbose_name="Category"
+        verbose_name_plural="Categories"
+
+
+    def __str__(self):
+        return self.CATName
