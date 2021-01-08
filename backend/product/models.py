c8fc006e16358308e017f9d470674a7c897ea3fd
Amyyne <tlijaniamine9@gmail.com>
2021-01-08 21:14:59 +0100

    Create product model

---
+++ backend/product/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    PRDName = models.CharField(max_length=100)
+    PRDDesc = models.TextField()
+    PRDPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    PRDCost = models.DecimalField(max_digits=5, decimal_places=2)
+    PRDCreated = models.DateTimeField()
